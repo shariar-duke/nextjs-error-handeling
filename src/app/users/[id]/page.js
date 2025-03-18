bf2b8e65f75e8b418bd756367cbd38abcac8e616
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-18 19:19:48 +0600

    api called

--- src/app/users/[id]/page.js
+++ src/app/users/[id]/page.js
@@ -1,4 +1,7 @@
+import { getUserDetails } from "../../../../lib/user";
 export default function UserDetailsPage({ params: { id } }) {
+  const UserDetailsPage = getUserDetails(id);
+  console.log("The user details are", UserDetailsPage);
   return (
     <div className="p-2">
       <h1 className="text-3xl my-2"> User Details Page </h1>
