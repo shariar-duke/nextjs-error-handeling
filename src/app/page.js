c17f448987481f97a5af2a0dbbf0d29f46d5dafe
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-18 19:24:00 +0600

    home page modified

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <p>Shariar Mahamud Duke</p>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex space-x-4 bg-white p-6 shadow-lg rounded-lg">
+        <Link
+          className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
+          href="/courses"
+        >
+          Courses
+        </Link>
+        <Link
+          className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition"
+          href="/users"
+        >
+          Users
+        </Link>
+      </div>
     </div>
   );
 }
