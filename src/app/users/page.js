13e57f2e3f8f40311e1a883edb31a6e1f6246ded
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-18 23:10:02 +0600

    error thrown in user page

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import { getSession } from "../../../lib/auth";
 
-export default function Page() {
+export default async function Page() {
+  const session = await getSession();
+  console.log(session);
+
+  if (!session) {
+    throw new Error("No Session info available");
+  }
   return (
     <div className="p-2">
       <h1 className="my-2 text-3xl">User List Page</h1>
